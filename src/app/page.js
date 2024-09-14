7d03c743facb1aca6839cc506de73d9ad029a5bf
Kamrul Hassan <kamrulthedev@gmail.com>
2024-09-14 18:06:29 +0600

    user-state-and-set-counter

--- src/app/page.js
+++ src/app/page.js
@@ -1,19 +1,30 @@
-import Link from "next/link";
-import React from "react";
+"use client"
+import React, { useState } from "react";
 
 const HomePage = () => {
+
+const [counter, setCounter] = useState(0)
+console.log("Hello Wold")
+
   return (
     <div className="bg-zinc-500 h-screen">
       <h1 className="text-2xl font-serif text-center uppercase text-black">
         This is My First Next.js Application
       </h1>
+      <h1 className="text-center text-black mt-4 text-2xl font-serif">Counter : {counter}</h1>
       <div className="text-center mt-10">
-        <Link
-          href={"/news/blog/blog1"}
+        <button
+        onClick={()=> setCounter(counter + 1)}
+          className="uppercase text-center m-6 font-serif bg-green-500 text-black p-3 rounded-lg"
+        >
+          increase
+        </button>
+        <button
+           onClick={()=> setCounter(counter - 1)}
           className="uppercase text-center font-serif bg-green-500 text-black p-3 rounded-lg"
         >
-          Blog Page
-        </Link>
+          derease
+        </button>
       </div>
     </div>
   );
