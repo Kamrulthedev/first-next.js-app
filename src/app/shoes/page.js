e1bb554d3ce30f4c5b4d00fd295aad1c6332137e
Kamrul Hassan <kamrulthedev@gmail.com>
2024-09-18 16:24:53 +0600

    update-commit

--- src/app/shoes/page.js
+++ src/app/shoes/page.js
@@ -1,23 +1,19 @@
-import Image from "next/image";
-import Counter from "./components/counter/Counter";
-import Link from "next/link";
+import Image from 'next/image';
+import React from 'react';
 
-const HomePage = async () => {
-  const res = await fetch("http://localhost:5000/shoes", {
-    next: {
-      revalidate: 5,
-    },
-  });
-  const shoes = await res.json();
+const AllShoes = async() => {
+    const res = await fetch("http://localhost:5000/shoes", {
+        next: {
+          revalidate: 5,
+        },
+      });
+      const shoes = await res.json();
 
-  return (
-    <div className=" h-screen ">
-      <h1 className="text-2xl font-serif text-center uppercase text-white">
-        This is My First Next.js Application
-      </h1>
-      <Counter></Counter>
-      <div className="grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2 gap-6">
-        {shoes.slice(3).map((shoe) => (
+    return (
+        <div>
+            <h1 className='text-3xl font-serif text-center items-center'>All Shoes Page</h1>
+         <div className="grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2 gap-6">
+        {shoes.map((shoe) => (
           <div key={shoe.id} className="flex">
             <div className="card bg-base-100 w-96 shadow-xl p-5">
               <figure>
@@ -43,11 +39,8 @@ const HomePage = async () => {
           </div>
         ))}
       </div>
-      <div className="flex justify-center p-5">
-          <Link href='/shoes' className="font-serif  p-3 rounded-lg bg-green-500 text-black text-2xl">SEE MORE</Link>
         </div>
-    </div>
-  );
+    );
 };
 
-export default HomePage;
+export default AllShoes;
